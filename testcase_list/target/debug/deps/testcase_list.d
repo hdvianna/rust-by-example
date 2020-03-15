aedacf210e1c4e662f31ccc83bd7d7f171edff31
hdvianna <hdvianna@gmail.com>
2020-03-15 17:10:42 -0300

    added testcase_list

---
+++ testcase_list/target/debug/deps/testcase_list.d
@@ -0,0 +1,5 @@
+C:\Users\henrique\Desktop\backup-note\henrique\Dev\rust-by-example\testcase_list\target\debug\deps\testcase_list.exe: src\main.rs
+
+C:\Users\henrique\Desktop\backup-note\henrique\Dev\rust-by-example\testcase_list\target\debug\deps\testcase_list.d: src\main.rs
+
+src\main.rs:
---
+++ testcase_list/target/debug/testcase_list.d
@@ -0,0 +1,1 @@
+C:\Users\henrique\Desktop\backup-note\henrique\Dev\rust-by-example\testcase_list\target\debug\testcase_list.exe: C:\Users\henrique\Desktop\backup-note\henrique\Dev\rust-by-example\testcase_list\src\main.rs
