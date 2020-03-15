a39b8e9a40531541862d1881846cae0ecaa4ce70
hdvianna <hdvianna@gmail.com>
2020-03-15 16:19:54 -0300

    first examples

---
+++ hello-world/src/main.rs
@@ -0,0 +1,6 @@
+//https://doc.rust-lang.org/stable/rust-by-example/hello.html
+fn main() {
+    //println! is a macro that prints text to the console.
+    println!("Hello, world!");
+    println!("I'm a Rustacean!");
+}
